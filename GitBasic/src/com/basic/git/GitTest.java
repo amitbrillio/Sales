e29767e3d2d91b802d6574b8e46fd487919d0386
Amit <Amit@LAPTOP-AC3SP1OG>
2016-11-25 21:46:12 +0530

    Repository  intial commit

---
+++ GitBasic/src/com/basic/git/GitTest.java
@@ -0,0 +1,19 @@
+package com.basic.git;
+
+public class GitTest {
+	
+	public int sum(int a,int b) {
+		
+		return a+b ;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GitTest gt=new GitTest();
+		int a= gt.sum(10, 20);
+		System.out.println("sum is"+a);
+		
+	}
+
+}
